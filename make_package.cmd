d9e51ab85022722fa1201a44a7cc2ae2c22678f0
Yota Ichino <nusichino@users.sourceforge.jp>
2011-05-07 19:04:08 +0900

    パッケージ作成のバッチファイルを修正
    Google Chromeの実行ファイルパスが別の場所に変更されていたので
    それに対応させた。

--- make_package.cmd
+++ make_package.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set CHROME_PATH="C:\Program Files\Google\Chrome\Application"
+set CHROME_PATH="%HOME%\AppData\Local\Google\Chrome\Application"
 set EXTENSION_DIR=%CD%\NC4GC
 set EXTENSION_KEY_FILE=%CD%\NC4GC.pem
 
